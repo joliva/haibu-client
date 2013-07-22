7b6cc32f4c58cc55966212c5cf6c29f9afb1721e
John Oliva <joliva63@gmail.com>
2013-07-22 19:01:19 -0400

    converted to use of restler

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 #!/usr/bin/env node
 
+var rest = require('restler');
 var http = require('http');
 var program = require('commander');
 var pretty = require('prettyjson');
-var rest = require('restler');
+
+var host, port, config;
+
+function set_globals() {
+	host = program.host || '127.0.0.1';
+	port = program.port || 9002;
+	config = program.config ? JSON.parse(program.config) : {};
+
+	if (program.verbose === true) {
+		console.log('options:')
+		console.log('  host:' + host);
+		console.log('  port:' + port);
+		console.log('  config:' + JSON.stringify(config));
+	}
+}
+
+function netget(path, callback) {
+	set_globals();
+
+	var uri = 'http://' + host + ':' + port.toString() + path;
+	
+	rest.get(uri).on('complete', function(result, response) {
+		if (result instanceof Error) {
+			console.error('problem with request: ' + response.message);
+			return;
+		} else {
+			callback(result);
+		}
+	});
+}
+
+function do_version() {
+	netget('/version', function(result) {
+		console.log('service version: \n' + pretty.render(result));
+	});
+}
+
+function do_running() {
+	netget('/drones/running', function(result) {
+		console.log('running drones: \n' + pretty.render(result));
+	});
+}
 
 program
 	.usage('[options] command')
@@ -12,87 +54,19 @@ program
 	.option('-c, --config [config]', 'Config file')
 	.option('-v, --verbose', 'Verbose output');
 
-/*
 program
 	.command('version')
 	.description('show service version')
 	.action(function() {
-  		
-		set_globals(program.verbose);
-
-		var options = {
-			method: 'GET',
-			host: host,
-			port: port,
-			path: '/version'
-		}
-
-		var req = http.request(options, function(resp) {
-			var data ='';
-
-			resp.on('data', function(chunk) {
-				data += chunk;
-			});
-
-			resp.on('end', function() {
-				console.log('service version: \n' + pretty.render(JSON.parse(data)));
-			});
-		});
-
-		req.end();
-
-		req.on('error', function(e) {
-			console.log('problem with request: ' + e.message);
-		});
-  });
-*/
+  		do_version();
+	});
 
 program
 	.command('running')
 	.description('show running drones')
 	.action(function() {
-		set_globals(program.verbose);
-
-		var options = {
-			method: 'GET',
-			host: host,
-			port: port,
-			path: '/drones/running'
-		}
-
-		var req = http.request(options, function(resp) {
-			var data ='';
-
-			resp.on('data', function(chunk) {
-				data += chunk;
-			});
-
-			resp.on('end', function() {
-				console.log('running drones: \n' + pretty.render(JSON.parse(data)));
-			});
-		});
-
-		req.end();
-
-		req.on('error', function(e) {
-			console.log('problem with request: ' + e.message);
-		});
-  });
+  		do_running();
+	});
 
 program.parse(process.argv);
-
-function set_globals(verbose) {
-	host = program.host || '127.0.0.1';
-	port = program.port || 9002;
-	config = program.config ? JSON.parse(program.config) : {};
-
-	if (verbose === true) {
-		console.log('options:')
-		console.log('  host:' + host);
-		console.log('  port:' + port);
-		console.log('  config:' + JSON.stringify(config));
-	}
-}
-
-var host, port, config;
 
